e07fbf7b31f513d677191852878d4c881c307111
liuchuancong <chuancong@wangdou-hbean.com>
2026-04-07 09:04:51 +0800

    修复炸麦

--- lib/player/media_kit_adapter.dart
+++ lib/player/media_kit_adapter.dart
@@ -53,9 +53,10 @@ class MediaKitPlayerAdapter implements UnifiedPlayer {
 
       // 1. 设置协议白名单
       await native.setProperty('protocol_whitelist', 'httpproxy,udp,rtp,tcp,tls,data,file,http,https,crypto');
+      await native.setProperty('demuxer-lavf-probsize', '2097152'); // 减半探测大小
+      await native.setproperty('audio-buffer', '0.5');
+      await native.setProperty('demuxer-lavf-analyzeduration', '10'); // 减少解析时间
       await native.setProperty('network-timeout', '30'); // 给 mpv 30秒的总容忍时间
-      await native.setProperty('demuxer-lavf-probsize', '1048576'); // 减半探测大小
-      await native.setProperty('demuxer-lavf-analyzeduration', '3'); // 减少解析时间
     }
     if (settings.customPlayerOutput.value) {
       if (_player.platform is NativePlayer) {
